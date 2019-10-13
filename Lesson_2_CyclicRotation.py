97e681325a2526c6a788122b748f633465bd1e8b
ataabbasi <46480682+ataabbasi@users.noreply.github.com>
2019-10-13 13:34:38 +0200

    Update Lesson_2_CyclicRotation.py

--- Lesson_2_CyclicRotation.py
+++ Lesson_2_CyclicRotation.py
@@ -1,2 +1,35 @@
 # https://app.codility.com/programmers/lessons/2-arrays/cyclic_rotation/
 
+def solution(A, K):    
+    m=len(A)
+    if m<2 or K==0:
+        return A
+    else:
+        K%=m                 # suppose A= [3,7] and K=98
+        B=A.copy()        
+        for i in range(m):
+            if (i+K)<m:
+                A[i+K]=B[i]                
+            else:
+                A[i+K-m]=B[i]    
+        return A
+# Testing
+A0=[]
+A1=[1]
+A2=[1,2]
+A3=[1,2,3]
+A4=[1,2,3,4]
+
+K0=0
+K1=1
+K2=2
+K3=3
+K4=4
+K5=5
+K6=6
+K7=7
+K8=8
+
+A=A4
+K=K6
+solution(A, K)
