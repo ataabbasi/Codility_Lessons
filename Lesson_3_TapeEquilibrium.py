f5f3267314f3d6ee47a72e48a8d5e0241936701c
ataabbasi <46480682+ataabbasi@users.noreply.github.com>
2019-10-14 11:29:35 +0200

    Update Lesson_3_TapeEquilibrium.py

--- Lesson_3_TapeEquilibrium.py
+++ Lesson_3_TapeEquilibrium.py
@@ -1,1 +1,18 @@
 # https://app.codility.com/programmers/lessons/3-time_complexity/tape_equilibrium/
+
+def solution(A):
+    m=len(A)
+    result=[0]*(m-1)          # faster than append
+    a=0
+    b=sum(A)
+    for P in range(m-1):
+        a+=A[P]
+        b-=A[P]
+        result[P]=abs(a-b) 
+    return min(result)
+
+# testing
+tempA=[[1,2],[2,1],[1,1,1],[-1,1],[1,3,5,7],[-2,-4,5,7,9]]
+for i in range(len(tempA)):
+    A=tempA[i]
+    print('A = ',A, '\t\tResult =', solution(A))
