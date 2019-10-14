2ecc96936f9fa58004f1a147f93b115a73263320
ataabbasi <46480682+ataabbasi@users.noreply.github.com>
2019-10-14 11:46:34 +0200

    Update Lesson_4_PermCheck.py

--- Lesson_4_PermCheck.py
+++ Lesson_4_PermCheck.py
@@ -1,1 +1,15 @@
 # https://app.codility.com/programmers/lessons/4-counting_elements/perm_check/
+
+def solution(A):
+    A.sort()
+    N=len(A)
+    for i in range(N):
+        if A[i]!=i+1:
+            return 0
+    return 1
+  
+# Testing
+tempA=[[1,2],[2,1],[1,2,3],[3,1],[1,3,2,4],[1,4,1,4,5]]
+for i in range(len(tempA)):
+    A=tempA[i]
+    print('A = ',A, '\t\tResult =', solution(A))
